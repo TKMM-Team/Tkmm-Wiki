d6956bd50e3c007406afa4a055b62dc716842c94
ArchLeaders <archleadership28@gmail.com>
2024-02-23 12:51:20 -0800

    Update site styling

---
+++ wwwroot/templates/custom/public/main.js
@@ -0,0 +1,14 @@
+export default {
+    iconLinks: [
+        {
+            icon: 'github',
+            href: 'https://github.com/TKMM-Team',
+            title: 'GitHub'
+        },
+        {
+            icon: 'discord',
+            href: 'https://discord.gg/BbVXenRFVc',
+            title: 'Discord'
+        }
+    ]
+}
